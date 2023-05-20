93868106d8d5a404ffc505346ed9153b852398eb
Hauke Hund <hauke.hund@hs-heilbronn.de>
2023-05-20 03:03:54 +0200

    plugin name, release date

--- src/main/java/dev/dsf/bpe/PingProcessPluginDefinition.java
+++ src/main/java/dev/dsf/bpe/PingProcessPluginDefinition.java
@@ -11,12 +11,12 @@ import dev.dsf.bpe.v1.ProcessPluginDefinition;
 public class PingProcessPluginDefinition implements ProcessPluginDefinition
 {
 	public static final String VERSION = "1.0.0.0";
-	public static final LocalDate RELEASE_DATE = LocalDate.of(2023, 4, 22);
+	public static final LocalDate RELEASE_DATE = LocalDate.of(2023, 5, 20);
 
 	@Override
 	public String getName()
 	{
-		return "dsf-bpe-process-ping";
+		return "dsf-process-ping-pong";
 	}
 
 	@Override
