70a899d6bc13b78ae7e1d7b0f27d89719da47ab9
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-07 14:47:19 +0100

    Fatping will be ping pong version 2.0

--- src/main/java/dev/dsf/bpe/PingProcessPluginDefinition.java
+++ src/main/java/dev/dsf/bpe/PingProcessPluginDefinition.java
@@ -10,7 +10,7 @@ import dev.dsf.bpe.v1.ProcessPluginDefinition;
 
 public class PingProcessPluginDefinition implements ProcessPluginDefinition
 {
-	public static final String VERSION = "1.0.1.0";
+	public static final String VERSION = "2.0.0.0";
 	public static final LocalDate RELEASE_DATE = LocalDate.of(2023, 9, 12);
 
 	@Override
