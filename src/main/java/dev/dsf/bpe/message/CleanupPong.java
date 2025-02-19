aa3264917411bb129df9931618d95e2696eb8762
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-19 14:37:03 +0100

    Added Cleanup Pong implementation class

---
+++ src/main/java/dev/dsf/bpe/message/CleanupPong.java
@@ -0,0 +1,26 @@
+package dev.dsf.bpe.message;
+
+import java.util.stream.Stream;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.hl7.fhir.r4.model.Task;
+
+import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.activity.AbstractTaskMessageSend;
+import dev.dsf.bpe.v1.variables.Variables;
+
+public class CleanupPong extends AbstractTaskMessageSend
+{
+	public CleanupPong(ProcessPluginApi api)
+	{
+		super(api);
+	}
+
+	@Override
+	protected Stream<Task.ParameterComponent> getAdditionalInputParameters(DelegateExecution execution,
+			Variables variables)
+	{
+		System.out.println("Getting additional input parameters for pong cleanup");
+		return super.getAdditionalInputParameters(execution, variables);
+	}
+}
