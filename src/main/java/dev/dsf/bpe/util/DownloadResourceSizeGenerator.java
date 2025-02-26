eef7b777018753e4d208cfa0c3e31f0bd2557410
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-26 17:01:18 +0100

    Ran format-and-sort

--- src/main/java/dev/dsf/bpe/util/DownloadResourceSizeGenerator.java
+++ src/main/java/dev/dsf/bpe/util/DownloadResourceSizeGenerator.java
@@ -11,7 +11,8 @@ public class DownloadResourceSizeGenerator
 	public static Task.ParameterComponent create(int sizeBytes)
 	{
 		Task.ParameterComponent param = new Task.ParameterComponent();
-		param.setValue(new IntegerType(sizeBytes)).getType().addCoding(new Coding(ConstantsPing.CODESYSTEM_DSF_PING, ConstantsPing.CODESYSTEM_DSF_PING_VALUE_DOWNLOAD_RESOURCE_SIZE_BYTES, null));
+		param.setValue(new IntegerType(sizeBytes)).getType().addCoding(new Coding(ConstantsPing.CODESYSTEM_DSF_PING,
+				ConstantsPing.CODESYSTEM_DSF_PING_VALUE_DOWNLOAD_RESOURCE_SIZE_BYTES, null));
 		return param;
 	}
 }
