ac107e4b9c6aee00e56b19e6504b8dcfdd1900c2
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-26 17:30:37 +0100

    Added missing beans

--- src/main/java/dev/dsf/bpe/spring/config/PingConfig.java
+++ src/main/java/dev/dsf/bpe/spring/config/PingConfig.java
@@ -9,15 +9,28 @@ import org.springframework.context.annotation.Scope;
 
 import dev.dsf.bpe.listener.SetCorrelationKeyListener;
 import dev.dsf.bpe.mail.ErrorMailService;
+import dev.dsf.bpe.message.CleanupPong;
 import dev.dsf.bpe.message.SendPing;
 import dev.dsf.bpe.message.SendPong;
 import dev.dsf.bpe.message.SendStartPing;
+import dev.dsf.bpe.service.Cleanup;
+import dev.dsf.bpe.service.DownloadResourceAndMeasureSpeed;
+import dev.dsf.bpe.service.EstimateCleanupTimerDuration;
+import dev.dsf.bpe.service.GenerateResource;
+import dev.dsf.bpe.service.LogAndSaveAndStoreError;
+import dev.dsf.bpe.service.LogAndSaveError;
+import dev.dsf.bpe.service.LogAndSaveNoResponse;
+import dev.dsf.bpe.service.LogAndSaveSendError;
+import dev.dsf.bpe.service.LogAndStoreSendError;
 import dev.dsf.bpe.service.LogNoResponse;
 import dev.dsf.bpe.service.LogPing;
 import dev.dsf.bpe.service.LogSendError;
+import dev.dsf.bpe.service.SaveDownloadSpeeds;
 import dev.dsf.bpe.service.SelectPingTargets;
 import dev.dsf.bpe.service.SelectPongTarget;
 import dev.dsf.bpe.service.SetTargetAndConfigureTimer;
+import dev.dsf.bpe.service.StoreDownloadSpeeds;
+import dev.dsf.bpe.service.StoreResource;
 import dev.dsf.bpe.service.StoreResults;
 import dev.dsf.bpe.util.PingStatusGenerator;
 import dev.dsf.bpe.v1.ProcessPluginApi;
@@ -133,5 +146,96 @@ public class PingConfig
 	public SendPong sendPong()
 	{
 		return new SendPong(api, responseGenerator(), errorLogger());
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public StoreResource storeResource()
+	{
+		return new StoreResource(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public LogAndSaveNoResponse logAndSaveNoResponse()
+	{
+		return new LogAndSaveNoResponse(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public CleanupPong cleanupPong()
+	{
+		return new CleanupPong(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public DownloadResourceAndMeasureSpeed downloadResourceAndMeasureSpeed()
+	{
+		return new DownloadResourceAndMeasureSpeed(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public GenerateResource generateResource()
+	{
+		return new GenerateResource(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public LogAndSaveSendError logAndSaveSendError()
+	{
+		return new LogAndSaveSendError(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public LogAndStoreSendError logAndStoreSendError()
+	{
+		return new LogAndStoreSendError(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Cleanup cleanup()
+	{
+		return new Cleanup(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public LogAndSaveAndStoreError logAndSaveAndStoreError()
+	{
+		return new LogAndSaveAndStoreError(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public LogAndSaveError logAndSaveError()
+	{
+		return new LogAndSaveError(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public SaveDownloadSpeeds saveDownloadSpeeds()
+	{
+		return new SaveDownloadSpeeds(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public StoreDownloadSpeeds storeDownloadSpeeds()
+	{
+		return new StoreDownloadSpeeds(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public EstimateCleanupTimerDuration estimateCleanupTimerDuration()
+	{
+		return new EstimateCleanupTimerDuration(api);
 	}
 }
