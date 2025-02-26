914ca24aa732f0caff6c408113acb6d55b03eb95
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-26 09:58:59 +0100

    Added helper class to create Input Parameters downloaded-duration-millis and downloaded-bytes

---
+++ src/main/java/dev/dsf/bpe/util/NetworkSpeedMetricGenerator.java
@@ -0,0 +1,29 @@
+package dev.dsf.bpe.util;
+
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.DecimalType;
+import org.hl7.fhir.r4.model.IntegerType;
+import org.hl7.fhir.r4.model.Task;
+
+import dev.dsf.bpe.ConstantsPing;
+
+public class NetworkSpeedMetricGenerator
+{
+	public Task.ParameterComponent createDownloadedDurationMillis(long duration)
+	{
+		Task.ParameterComponent downloadedDuration = new Task.ParameterComponent();
+		downloadedDuration.setValue(new DecimalType(duration)).getType().addCoding(
+				new Coding(ConstantsPing.CODESYSTEM_DSF_PING,
+						ConstantsPing.CODESYSTEM_DSF_PING_VALUE_DOWNLOADED_DURATION_MILLIS, null));
+		return downloadedDuration;
+	}
+
+	public Task.ParameterComponent createDownloadedBytes(int bytes)
+	{
+		Task.ParameterComponent downloadedBytes = new Task.ParameterComponent();
+		downloadedBytes.setValue(new IntegerType(bytes)).getType().addCoding(
+				new Coding(ConstantsPing.CODESYSTEM_DSF_PING, ConstantsPing.CODESYSTEM_DSF_PING_VALUE_DOWNLOADED_BYTES,
+						null));
+		return downloadedBytes;
+	}
+}
