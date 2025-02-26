5aa5a562595ace1cfb150418496aa8b016ed711c
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-26 10:52:56 +0100

    Added util classes to create download-resource-reference and download-resource-size input parameters

---
+++ src/main/java/dev/dsf/bpe/util/DownloadResourceSizeGenerator.java
@@ -0,0 +1,17 @@
+package dev.dsf.bpe.util;
+
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.IntegerType;
+import org.hl7.fhir.r4.model.Task;
+
+import dev.dsf.bpe.ConstantsPing;
+
+public class DownloadResourceSizeGenerator
+{
+	public static Task.ParameterComponent create(int sizeBytes)
+	{
+		Task.ParameterComponent param = new Task.ParameterComponent();
+		param.setValue(new IntegerType(sizeBytes)).getType().addCoding(new Coding(ConstantsPing.CODESYSTEM_DSF_PING, ConstantsPing.CODESYSTEM_DSF_PING_VALUE_DOWNLOAD_RESOURCE_SIZE_BYTES, null));
+		return param;
+	}
+}
