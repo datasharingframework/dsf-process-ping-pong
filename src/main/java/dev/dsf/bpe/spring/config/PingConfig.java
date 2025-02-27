f85c1f7d529530fd0c804a0b683c433016713463
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-27 13:23:58 +0100

    Sending to non-subprocess requires no correlation-key to be present, otherwise DSF will try to correlate a process instance with the correlation key. This required the target to be recreated. This was done using an execution listener

--- src/main/java/dev/dsf/bpe/spring/config/PingConfig.java
+++ src/main/java/dev/dsf/bpe/spring/config/PingConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import dev.dsf.bpe.listener.RemoveCorrelationKeyListener;
 import dev.dsf.bpe.listener.SetCorrelationKeyListener;
 import dev.dsf.bpe.mail.ErrorMailService;
 import dev.dsf.bpe.message.CleanupPong;
@@ -253,5 +254,12 @@ public class PingConfig
 	public SavePong savePong()
 	{
 		return new SavePong(api);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public RemoveCorrelationKeyListener removeCorrelationKeyListener()
+	{
+		return new RemoveCorrelationKeyListener(api);
 	}
 }
