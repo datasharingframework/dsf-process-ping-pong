4b56e0f1d3d91f51251e577f2f2b1220632f21a5
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-27 20:50:21 +0100

    Added resource cleanup

--- src/main/java/dev/dsf/bpe/service/Cleanup.java
+++ src/main/java/dev/dsf/bpe/service/Cleanup.java
@@ -2,16 +2,18 @@ package dev.dsf.bpe.service;
 
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.hl7.fhir.r4.model.Binary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.dsf.bpe.ConstantsPing;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
 import dev.dsf.bpe.v1.variables.Variables;
 
 public class Cleanup extends AbstractServiceDelegate
 {
-	private final static Logger logger = LoggerFactory.getLogger(Cleanup.class.getName());
+	private final static Logger logger = LoggerFactory.getLogger(Cleanup.class);
 
 	public Cleanup(ProcessPluginApi api)
 	{
@@ -21,6 +23,10 @@ public class Cleanup extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
-		logger.info("Cleaning up");
+		String downloadResourceId = variables.getString(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOAD_RESOURCE_ID);
+		api.getFhirWebserviceClientProvider().getLocalWebserviceClient().delete(Binary.class, downloadResourceId);
+		api.getFhirWebserviceClientProvider().getLocalWebserviceClient().deletePermanently(Binary.class,
+				downloadResourceId);
+		logger.info("Cleanup completed. Deleted Binary resource with ID {}", downloadResourceId);
 	}
 }
