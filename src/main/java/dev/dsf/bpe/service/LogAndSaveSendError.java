a4d429044cc9f590bfe1e9f69687642359086e0d
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-28 15:20:12 +0100

    Added implementation

--- src/main/java/dev/dsf/bpe/service/LogAndSaveSendError.java
+++ src/main/java/dev/dsf/bpe/service/LogAndSaveSendError.java
@@ -5,8 +5,12 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.dsf.bpe.ConstantsPing;
+import dev.dsf.bpe.util.ErrorMessageListUtils;
+import dev.dsf.bpe.util.task.input.generator.ErrorMessageGenerator;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.variables.Target;
 import dev.dsf.bpe.v1.variables.Variables;
 
 public class LogAndSaveSendError extends AbstractServiceDelegate
@@ -21,6 +25,10 @@ public class LogAndSaveSendError extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
-		logger.info("Logging and saving error during sending");
+		String statusCode = (String) delegateExecution.getVariableLocal("statusCode");
+		Target target = variables.getTarget();
+		String correlationKey = target.getCorrelationKey();
+		variables.setString(ConstantsPing.BPMN_EXECUTION_VARIABLE_STATUS_CODE + "_" + correlationKey, statusCode);
+		logger.info("Request to {} resulted in status {}", target.getEndpointUrl(), statusCode);
 	}
 }
