e6baaf4c7e227ae4ee3b87a96572f73d6099d7d8
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-28 17:23:59 +0100

    Fixed logic error

--- src/main/java/dev/dsf/bpe/service/DownloadResourceAndMeasureSpeed.java
+++ src/main/java/dev/dsf/bpe/service/DownloadResourceAndMeasureSpeed.java
@@ -36,7 +36,7 @@ public class DownloadResourceAndMeasureSpeed extends AbstractServiceDelegate
 		BinaryResourceDownloader.DownloadResult downloadResult = new BinaryResourceDownloader().download(variables, api,
 				task, maxDownloadSizeBytes);
 
-		if (downloadResult.getErrorMessage() != null)
+		if (downloadResult.getErrorMessage() == null)
 		{
 			variables.setInteger(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOADED_BYTES,
 					downloadResult.getDownloadedBytes());
