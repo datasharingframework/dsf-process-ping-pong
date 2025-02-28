98d4bab5f64620d02aa9c92bd5cf3a662e3fff57
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-02-28 16:21:13 +0100

    Moved more common functionality in BinaryResourceDownloader

--- src/main/java/dev/dsf/bpe/service/DownloadResourceAndMeasureSpeedInSubProcess.java
+++ src/main/java/dev/dsf/bpe/service/DownloadResourceAndMeasureSpeedInSubProcess.java
@@ -1,14 +1,9 @@
 package dev.dsf.bpe.service;
 
 import static dev.dsf.bpe.ConstantsPing.BPMN_EXECUTION_VARIABLE_ERROR_MESSAGE_LIST;
-import static dev.dsf.bpe.ConstantsPing.WEBSERVICE_URL_PATTERN;
-
-import java.io.InputStream;
-import java.util.regex.Matcher;
 
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,52 +34,22 @@ public class DownloadResourceAndMeasureSpeedInSubProcess extends AbstractService
 		Target target = variables.getTarget();
 		String correlationKey = target.getCorrelationKey();
 
-		int downloadResourceSizeBytes = variables
-				.getInteger(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOAD_RESOURCE_SIZE_BYTES);
+		String webserviceUrl = target.getEndpointUrl();
 
-		Reference downloadResourceReference = api.getTaskHelper()
-				.getFirstInputParameterValue(task, ConstantsPing.CODESYSTEM_DSF_PING,
-						ConstantsPing.CODESYSTEM_DSF_PING_VALUE_DOWNLOAD_RESOURCE_REFERENCE, Reference.class)
-				.orElseThrow();
+		BinaryResourceDownloader.DownloadResult downloadResult = new BinaryResourceDownloader().download(webserviceUrl,
+				variables, api, task, maxDownloadSizeBytes);
 
-		Matcher matcher = WEBSERVICE_URL_PATTERN.matcher(downloadResourceReference.getReference());
-		boolean matched = matcher.find();
-		if (matched)
+		if (downloadResult.getErrorMessage() != null)
 		{
-			String webserviceUrl = matcher.group(1);
-
-			InputStream binaryResourceInputStream = api.getFhirWebserviceClientProvider()
-					.getWebserviceClient(webserviceUrl).readBinary(getDownloadResourceId(downloadResourceReference),
-							ConstantsPing.DOWNLOAD_RESOURCE_MIME_TYPE);
-
-			BinaryResourceDownloader.DownloadResult downloadResult = new BinaryResourceDownloader().download(
-					binaryResourceInputStream, downloadResourceReference.getReference(), downloadResourceSizeBytes,
-					maxDownloadSizeBytes);
-
-			if (downloadResult.getErrorMessage() != null)
-			{
-				variables.setInteger(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOADED_BYTES,
-						downloadResult.getDownloadedBytes());
-				variables.setLong(
-						ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOADED_DURATION_MILLIS + "_" + correlationKey,
-						downloadResult.getDownloadedDurationMillis());
-			}
-			else
-			{
-				ErrorMessageListUtils.add(downloadResult.getErrorMessage(),
-						BPMN_EXECUTION_VARIABLE_ERROR_MESSAGE_LIST + "_" + correlationKey, delegateExecution);
-			}
+			variables.setInteger(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOADED_BYTES,
+					downloadResult.getDownloadedBytes());
+			variables.setLong(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOADED_DURATION_MILLIS + "_" + correlationKey,
+					downloadResult.getDownloadedDurationMillis());
 		}
 		else
 		{
-			ErrorMessageListUtils.add("Invalid download resource url:" + downloadResourceReference.getReference(),
+			ErrorMessageListUtils.add(downloadResult.getErrorMessage(),
 					BPMN_EXECUTION_VARIABLE_ERROR_MESSAGE_LIST + "_" + correlationKey, delegateExecution);
 		}
-	}
-
-	private String getDownloadResourceId(Reference downloadResourceReference)
-	{
-		String[] split = downloadResourceReference.getReference().split("/");
-		return split[split.length - 1];
 	}
 }
