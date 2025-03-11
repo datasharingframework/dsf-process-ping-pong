ef16c8d6f8e81c229c6b16d308d7a91e4fa77ebc
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-11 14:11:09 +0100

    Added estimation based on 10x the download duration

--- src/main/java/dev/dsf/bpe/service/EstimateCleanupTimerDuration.java
+++ src/main/java/dev/dsf/bpe/service/EstimateCleanupTimerDuration.java
@@ -1,10 +1,13 @@
 package dev.dsf.bpe.service;
 
+import java.time.Duration;
+
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.dsf.bpe.ConstantsPing;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
 import dev.dsf.bpe.v1.variables.Variables;
@@ -21,7 +24,12 @@ public class EstimateCleanupTimerDuration extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
+		long downloadedDurationMillis = variables.getLong(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOADED_DURATION_MILLIS);
+		long timerDurationMillis = downloadedDurationMillis > Long.MAX_VALUE / 10 ? Long.MAX_VALUE : downloadedDurationMillis * 10;
+
+		String cleanUpTimerDuration = Duration.ofMillis(timerDurationMillis).toString();
+		variables.setString("cleanupTimerDuration", cleanUpTimerDuration);
+
 		logger.info("Estimated cleanup timer duration");
-		variables.setString("cleanupTimerDuration", "PT20S");
 	}
 }
