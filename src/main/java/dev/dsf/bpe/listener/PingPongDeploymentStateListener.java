0539831cf3c07697ffb1c53d5b8a297a1a1144d8
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-12 18:22:33 +0100

    Added PingPongDeploymentStateListener to validate networkSpeedUnit environment variable and removed the service delegate LogSendError which was forgotten in a previous commit

---
+++ src/main/java/dev/dsf/bpe/listener/PingPongDeploymentStateListener.java
@@ -0,0 +1,45 @@
+package dev.dsf.bpe.listener;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+import dev.dsf.bpe.ConstantsPing;
+import dev.dsf.bpe.spring.config.PingConfig;
+import dev.dsf.bpe.v1.ProcessPluginDeploymentStateListener;
+
+public class PingPongDeploymentStateListener implements ProcessPluginDeploymentStateListener, InitializingBean
+{
+	private static final Logger logger = LoggerFactory.getLogger(PingPongDeploymentStateListener.class);
+
+	private final PingConfig pingConfig;
+	private final String networkSpeedUnit;
+
+	public PingPongDeploymentStateListener(PingConfig pingConfig)
+	{
+		this.pingConfig = pingConfig;
+		this.networkSpeedUnit = pingConfig.getNetworkSpeedUnit();
+	}
+
+	@Override
+	public void onProcessesDeployed(List<String> processes)
+	{
+		if (ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUES.contains(networkSpeedUnit))
+		{
+			logger.info("Network speed unit is valid: {}", networkSpeedUnit);
+		} else
+		{
+			pingConfig.setNetworkSpeedUnit(ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_MEGABYTES_PER_SECOND);
+			logger.info("Network speed unit \"{}\" is not valid. Valid values are: {}. Defaulting to \"{}\"", networkSpeedUnit, ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUES, ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_MEGABYTES_PER_SECOND);
+		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception
+	{
+		Objects.requireNonNull(networkSpeedUnit);
+	}
+}
