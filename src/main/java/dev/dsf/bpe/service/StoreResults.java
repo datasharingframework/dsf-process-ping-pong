dae78db1d105ed46d5719831358ac49e93b45984
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-12 18:45:08 +0100

    Now adds information on network speeds to task output

--- src/main/java/dev/dsf/bpe/service/StoreResults.java
+++ src/main/java/dev/dsf/bpe/service/StoreResults.java
@@ -1,5 +1,6 @@
 package dev.dsf.bpe.service;
 
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.InitializingBean;
 
 import dev.dsf.bpe.ConstantsPing;
 import dev.dsf.bpe.mail.ErrorMailService;
+import dev.dsf.bpe.util.ErrorMessageListUtils;
+import dev.dsf.bpe.util.task.NetworkSpeedCalculator;
 import dev.dsf.bpe.util.task.output.generator.PingStatusGenerator;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
@@ -21,11 +24,12 @@ import dev.dsf.bpe.v1.variables.Variables;
 public class StoreResults extends AbstractServiceDelegate implements InitializingBean
 {
 	private final ErrorMailService errorMailService;
+	private final String networkSpeedUnit;
 
-	public StoreResults(ProcessPluginApi api, ErrorMailService errorMailService)
+	public StoreResults(ProcessPluginApi api, ErrorMailService errorMailService, String networkSpeedUnit)
 	{
 		super(api);
-
+		this.networkSpeedUnit = networkSpeedUnit;
 		this.errorMailService = errorMailService;
 	}
 
@@ -62,6 +66,23 @@ public class StoreResults extends AbstractServiceDelegate implements Initializin
 			}
 			else
 			{
+				int downloadResourceSizeBytes = variables.getInteger(ConstantsPing.BPMN_EXECUTION_VARIABLE_DOWNLOAD_RESOURCE_SIZE_BYTES);
+				List<String> errorMessageList = ErrorMessageListUtils.getErrorMessageList(execution);
+				if (downloadResourceSizeBytes >= 0)
+				{
+					int downloadedBytes = variables.getInteger(ConstantsPing.getBpmnExecutionVariableDownloadedBytes(correlationKey));
+					long downloadedDurationMillis = variables.getLong(ConstantsPing.getBpmnExecutionVariableDownloadedDurationMillis(correlationKey));
+
+					BigDecimal downloadSpeed = NetworkSpeedCalculator.calculate(downloadedBytes, downloadedDurationMillis, networkSpeedUnit);
+
+					int uploadedBytes = variables.getInteger(ConstantsPing.getBpmnExecutionVariableUploadedBytes(correlationKey));
+					long uploadedDurationMillis = variables.getLong(ConstantsPing.getBpmnExecutionVariableUploadedDurationMillis(correlationKey));
+
+					BigDecimal uploadSpeed = NetworkSpeedCalculator.calculate(uploadedBytes, uploadedDurationMillis, networkSpeedUnit);
+
+					task.addOutput(PingStatusGenerator.createPingStatusOutput(target, statusCode, errorMessageList, downloadSpeed, uploadSpeed, networkSpeedUnit));
+				}
+
 				task.addOutput(PingStatusGenerator.createPingStatusOutput(target, statusCode));
 
 				if (ConstantsPing.CODESYSTEM_DSF_PING_STATUS_VALUE_PONG_MISSING.equals(statusCode))
