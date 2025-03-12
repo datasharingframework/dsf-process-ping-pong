1fef2e0b022f44e1f24aacb0082004029de991be
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-12 18:27:07 +0100

    Added storage logic

--- src/main/java/dev/dsf/bpe/service/LogAndStoreSendError.java
+++ src/main/java/dev/dsf/bpe/service/LogAndStoreSendError.java
@@ -1,12 +1,18 @@
 package dev.dsf.bpe.service;
 
+import java.util.List;
+
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.dsf.bpe.ConstantsPing;
+import dev.dsf.bpe.util.task.output.generator.PingStatusGenerator;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.variables.Target;
 import dev.dsf.bpe.v1.variables.Variables;
 
 public class LogAndStoreSendError extends AbstractServiceDelegate
@@ -21,6 +27,16 @@ public class LogAndStoreSendError extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
+		String statusCode = variables.getString(ConstantsPing.getBpmnExecutionVariableStatusCode());
+		String errorMessage = variables.getString(ConstantsPing.getBpmnExecutionVariableErrorMessage());
+		Target target = variables.getTarget();
 
+		Task startTask = variables.getStartTask();
+		Task.TaskOutputComponent pongStatus = PingStatusGenerator.createPongStatusOutput(variables.getTarget(),
+				statusCode, List.of(errorMessage));
+		startTask.addOutput(pongStatus);
+		variables.updateTask(startTask);
+		logger.error("Error {} when trying to download resource from {}: {}", statusCode,
+				target.getEndpointIdentifierValue(), errorMessage);
 	}
 }
