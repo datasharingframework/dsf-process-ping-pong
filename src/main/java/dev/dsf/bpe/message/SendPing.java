b4f0ae50b93e54e03de1f7c0efc08e7b64ec0ecd
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-12 18:15:55 +0100

    Removed LogSendError task as Ping now logs errors itself

--- src/main/java/dev/dsf/bpe/message/SendPing.java
+++ src/main/java/dev/dsf/bpe/message/SendPing.java
@@ -8,12 +8,15 @@ import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.ResourceType;
 import org.hl7.fhir.r4.model.Task;
 import org.hl7.fhir.r4.model.Task.ParameterComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.ConstantsPing;
 import dev.dsf.bpe.util.task.input.generator.DownloadResourceReferenceGenerator;
 import dev.dsf.bpe.util.task.input.generator.DownloadResourceSizeGenerator;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractTaskMessageSend;
+import dev.dsf.bpe.v1.variables.Target;
 import dev.dsf.bpe.v1.variables.Variables;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
@@ -21,6 +24,8 @@ import jakarta.ws.rs.core.Response.StatusType;
 
 public class SendPing extends AbstractTaskMessageSend
 {
+	private static final Logger logger = LoggerFactory.getLogger(SendPing.class);
+
 	public SendPing(ProcessPluginApi api)
 	{
 		super(api);
@@ -50,14 +55,18 @@ public class SendPing extends AbstractTaskMessageSend
 	protected void handleIntermediateThrowEventError(DelegateExecution execution, Variables variables,
 			Exception exception, String errorMessage)
 	{
+		Target target = variables.getTarget();
+		String correlationKey = target.getCorrelationKey();
+
 		String statusCode = exception instanceof WebApplicationException w && w.getResponse() != null
 				&& w.getResponse().getStatus() == Response.Status.FORBIDDEN.getStatusCode()
 						? ConstantsPing.CODESYSTEM_DSF_PING_STATUS_VALUE_NOT_ALLOWED
 						: ConstantsPing.CODESYSTEM_DSF_PING_STATUS_VALUE_NOT_REACHABLE;
-		execution.setVariableLocal(ConstantsPing.BPMN_EXECUTION_VARIABLE_STATUS_CODE, statusCode);
 
+		variables.setString(ConstantsPing.getBpmnExecutionVariableStatusCode(correlationKey), statusCode);
 		String specialErrorMessage = createErrorMessage(exception);
-		execution.setVariableLocal(ConstantsPing.BPMN_EXECUTION_VARIABLE_ERROR_MESSAGE, specialErrorMessage);
+		variables.setString(ConstantsPing.getBpmnExecutionVariableErrorMessage(correlationKey), specialErrorMessage);
+		logger.info("Request to {} resulted in status {}", target.getEndpointUrl(), statusCode);
 	}
 
 	@Override
