af822d019471a20e83b92ad8dfb019dea4824530
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-14 10:55:38 +0100

    Added MathContext

--- src/main/java/dev/dsf/bpe/util/task/NetworkSpeedCalculator.java
+++ src/main/java/dev/dsf/bpe/util/task/NetworkSpeedCalculator.java
@@ -1,7 +1,7 @@
 package dev.dsf.bpe.util.task;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.math.MathContext;
 
 import dev.dsf.bpe.ConstantsPing;
 
@@ -9,19 +9,20 @@ public class NetworkSpeedCalculator
 {
 	public static BigDecimal calculate(int bytes, long duration, String unit)
 	{
+		MathContext mathContext = new MathContext(4);
 		return switch (unit)
 		{
-			case ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_BITS_PER_SECOND ->
-				new BigDecimal(bytes * 8L).divide(BigDecimal.valueOf(duration / 1000), RoundingMode.HALF_DOWN);
+			case ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_BITS_PER_SECOND -> new BigDecimal(bytes * 8L)
+					.divide(BigDecimal.valueOf(duration).divide(BigDecimal.valueOf(1000), mathContext), mathContext);
 			case ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_MEGABITS_PER_SECOND ->
-				new BigDecimal(bytes * 8L).divide(BigDecimal.valueOf(1000000), RoundingMode.UNNECESSARY)
-						.divide(BigDecimal.valueOf(duration / 1000), RoundingMode.HALF_DOWN);
-			case ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_BYTES_PER_SECOND ->
-				new BigDecimal(bytes).divide(BigDecimal.valueOf(duration / 1000), RoundingMode.HALF_DOWN);
+				new BigDecimal(bytes * 8L).divide(BigDecimal.valueOf(1000000), mathContext).divide(
+						BigDecimal.valueOf(duration).divide(BigDecimal.valueOf(1000), mathContext), mathContext);
+			case ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_BYTES_PER_SECOND -> new BigDecimal(bytes)
+					.divide(BigDecimal.valueOf(duration).divide(BigDecimal.valueOf(1000), mathContext), mathContext);
 			case ConstantsPing.CODESYSTEM_DSF_PING_UNITS_VALUE_MEGABYTES_PER_SECOND ->
-				new BigDecimal(bytes).divide(BigDecimal.valueOf(1000000), RoundingMode.UNNECESSARY)
-						.divide(BigDecimal.valueOf(duration / 1000), RoundingMode.HALF_DOWN);
-			default -> new BigDecimal(0);
+				new BigDecimal(bytes).divide(BigDecimal.valueOf(1000000), mathContext).divide(
+						BigDecimal.valueOf(duration).divide(BigDecimal.valueOf(1000), mathContext), mathContext);
+			default -> BigDecimal.ZERO;
 		};
 	}
 }
