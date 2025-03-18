87c23a754093f5200c27d9ee07884b1aeb54d471
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-18 16:18:49 +0100

    Revert "Now adds upload and download data with 0 as values if an error occurred because StoreResults expects those values to be set even if an error occurred"
    This reverts commit 789e449ae4c94a4ef44f88452b1989077bc0a005.

--- src/main/java/dev/dsf/bpe/service/ping/LogAndSaveError.java
+++ src/main/java/dev/dsf/bpe/service/ping/LogAndSaveError.java
@@ -5,10 +5,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dev.dsf.bpe.ConstantsPing;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
-import dev.dsf.bpe.v1.variables.Target;
 import dev.dsf.bpe.v1.variables.Variables;
 
 public class LogAndSaveError extends AbstractServiceDelegate
@@ -23,8 +21,6 @@ public class LogAndSaveError extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
-		String correlationKey = variables.getTarget().getCorrelationKey();
-		variables.setInteger(ConstantsPing.getBpmnExecutionVariableDownloadedBytes(correlationKey), 0);
-		variables.setLong(ConstantsPing.getBpmnExecutionVariableDownloadedDurationMillis(correlationKey), 0L);
+		logger.info("Logging and saving error");
 	}
 }
