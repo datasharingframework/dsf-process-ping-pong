f8fc853f15902c733239467b95e663845adacf11
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-19 13:16:01 +0100

    DownloadResourceAndMeasureSpeed and DownloadResourceAndMeasureSpeedInSubProcess now throw BpmnErrors like the bpmn diagrams suggest. Errors get handled in LogAndSaveError and LogAndSaveAndStoreError respectively. Also renamed the error codes in bpmn files and added them to ConstantsPing

--- src/main/java/dev/dsf/bpe/service/ping/LogAndSaveError.java
+++ src/main/java/dev/dsf/bpe/service/ping/LogAndSaveError.java
@@ -3,9 +3,12 @@ package dev.dsf.bpe.service.ping;
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 
+import dev.dsf.bpe.ConstantsPing;
+import dev.dsf.bpe.util.ErrorMessageListUtils;
 import dev.dsf.bpe.util.logging.PingPongLogger;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.variables.Target;
 import dev.dsf.bpe.v1.variables.Variables;
 
 public class LogAndSaveError extends AbstractServiceDelegate
@@ -19,7 +22,11 @@ public class LogAndSaveError extends AbstractServiceDelegate
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
 		PingPongLogger logger = new PingPongLogger(LogAndSaveError.class, variables.getStartTask());
+		Target target = variables.getTarget();
 
-		logger.info("Logging and saving error");
+		String errorMessage = variables.getString(ConstantsPing.BPMN_EXECUTION_VARIABLE_RESOURCE_DOWNLOAD_ERROR_MESSAGE);
+		ErrorMessageListUtils.add(errorMessage, delegateExecution, target.getCorrelationKey());
+
+		logger.info("Error while trying to download resource from {}: {}", target.getEndpointUrl(), errorMessage);
 	}
 }
--- src/main/java/dev/dsf/bpe/service/pong/LogAndSaveAndStoreError.java
+++ src/main/java/dev/dsf/bpe/service/pong/LogAndSaveAndStoreError.java
@@ -2,10 +2,15 @@ package dev.dsf.bpe.service.pong;
 
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.hl7.fhir.r4.model.Task;
 
+import dev.dsf.bpe.ConstantsPing;
+import dev.dsf.bpe.util.ErrorMessageListUtils;
 import dev.dsf.bpe.util.logging.PingPongLogger;
+import dev.dsf.bpe.util.task.output.generator.PingStatusGenerator;
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.variables.Target;
 import dev.dsf.bpe.v1.variables.Variables;
 
 public class LogAndSaveAndStoreError extends AbstractServiceDelegate
@@ -19,7 +24,14 @@ public class LogAndSaveAndStoreError extends AbstractServiceDelegate
 	protected void doExecute(DelegateExecution delegateExecution, Variables variables) throws BpmnError, Exception
 	{
 		PingPongLogger logger = new PingPongLogger(LogAndSaveAndStoreError.class, variables.getStartTask());
+		Target target = variables.getTarget();
+		Task startTask = variables.getStartTask();
 
-		logger.debug("Logging and saving and storing error");
+		String errorMessage = variables.getString(ConstantsPing.BPMN_EXECUTION_VARIABLE_RESOURCE_DOWNLOAD_ERROR_MESSAGE);
+		ErrorMessageListUtils.add(errorMessage, delegateExecution);
+		PingStatusGenerator.updatePongStatusOutput(startTask, ErrorMessageListUtils.getErrorMessageList(delegateExecution));
+		variables.updateTask(startTask);
+
+		logger.info("Error while trying to download resource from {}: {}", target.getEndpointUrl(), errorMessage);
 	}
 }
