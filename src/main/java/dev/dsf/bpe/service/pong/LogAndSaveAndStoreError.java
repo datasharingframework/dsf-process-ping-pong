19d34d562e211a7d371361a2d65d76966f2ef67b
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-06-25 11:59:32 +0200

    Fixed wrongly retrieving errors from previous step

--- src/main/java/dev/dsf/bpe/service/pong/LogAndSaveAndStoreError.java
+++ src/main/java/dev/dsf/bpe/service/pong/LogAndSaveAndStoreError.java
@@ -28,16 +28,13 @@ public class LogAndSaveAndStoreError extends AbstractServiceDelegate
 		Target target = variables.getTarget();
 		Task startTask = variables.getStartTask();
 
-		String errorMessage = variables.getString(ConstantsPing.BPMN_EXECUTION_VARIABLE_RESOURCE_DOWNLOAD_ERROR);
-		ProcessError error = new ProcessError(ConstantsPing.CODESYSTEM_DSF_PING_PROCESSES_VALUE_PONG,
-				ConstantsPing.CODESYSTEM_DSF_PING_PROCESS_STEPS_VALUE_DOWNLOAD_RESOURCE_AND_MEASURE_SPEED,
-				"Downloading binary resource from " + target.getEndpointUrl(), ConstantsPing.POTENTIAL_FIX_URL_DUMMY,
-				errorMessage);
+		ProcessError error = (ProcessError) delegateExecution
+				.getVariableLocal(ConstantsPing.BPMN_EXECUTION_VARIABLE_RESOURCE_DOWNLOAD_ERROR);
 
 		ErrorListUtils.add(error, delegateExecution);
 		PingStatusGenerator.updatePongStatusOutput(startTask, ErrorListUtils.getErrorMessageList(delegateExecution));
 		variables.updateTask(startTask);
 
-		logger.info("Error while trying to download resource from {}: {}", target.getEndpointUrl(), errorMessage);
+		logger.info("Error while trying to download resource from {}: {}", target.getEndpointUrl(), error.message());
 	}
 }
