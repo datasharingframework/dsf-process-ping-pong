4b63e32ceaac607d5708f71d4e8f7640e2ca24d4
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-23 16:38:06 +0200

    API v2 changes: - interface instead of superclass -> no constructor needed - Error handling method replaced by error handler implementation - Provide TaskSender instead of overriding sendTask method

--- src/main/java/dev/dsf/bpe/message/CleanupPongMessage.java
+++ src/main/java/dev/dsf/bpe/message/CleanupPongMessage.java
@@ -1,9 +1,10 @@
 package dev.dsf.bpe.message;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,89 +15,106 @@ import dev.dsf.bpe.ExecutionVariables;
 import dev.dsf.bpe.util.task.SendTaskErrorConverter;
 import dev.dsf.bpe.util.task.input.generator.DownloadedBytesGenerator;
 import dev.dsf.bpe.util.task.input.generator.DownloadedDurationGenerator;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.activity.AbstractTaskMessageSend;
-import dev.dsf.bpe.v1.variables.Target;
-import dev.dsf.bpe.v1.variables.Variables;
-import dev.dsf.bpe.variables.process_error.ProcessErrorValueImpl;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.MessageSendTask;
+import dev.dsf.bpe.v2.activity.task.DefaultTaskSender;
+import dev.dsf.bpe.v2.activity.task.TaskSender;
+import dev.dsf.bpe.v2.activity.values.SendTaskValues;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.error.MessageSendTaskErrorHandler;
+import dev.dsf.bpe.v2.variables.Target;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class CleanupPongMessage extends AbstractTaskMessageSend
+public class CleanupPongMessage implements MessageSendTask
 {
 	private static final Logger logger = LoggerFactory.getLogger(CleanupPongMessage.class);
 
-	public CleanupPongMessage(ProcessPluginApi api)
-	{
-		super(api);
-	}
-
 	@Override
-	protected Stream<Task.ParameterComponent> getAdditionalInputParameters(DelegateExecution execution,
-			Variables variables)
+	public List<Task.ParameterComponent> getAdditionalInputParameters(ProcessPluginApi api, Variables variables,
+			SendTaskValues sendTaskValues, Target target)
 	{
-		Target target = variables.getTarget();
 		String correlationKey = target.getCorrelationKey();
 		Long downloadedBytes = variables.getLong(ExecutionVariables.downloadedBytes.correlatedValue(correlationKey));
-		Duration downloadedDuration = (Duration) variables
+		Duration downloadedDuration = variables
 				.getVariable(ExecutionVariables.downloadedDuration.correlatedValue(correlationKey));
 
-		Stream<Task.ParameterComponent> downloadedBytesParameter = downloadedBytes != null
-				? Stream.of(DownloadedBytesGenerator.create(downloadedBytes))
-				: Stream.empty();
-		Stream<Task.ParameterComponent> downloadedDurationParameter = downloadedDuration != null
-				? Stream.of(DownloadedDurationGenerator.create(downloadedDuration))
-				: Stream.empty();
+		List<Task.ParameterComponent> additionalInputParameters = new ArrayList<>();
 
-		return Stream.of(downloadedBytesParameter, downloadedDurationParameter).flatMap(s -> s);
+		if (downloadedBytes != null)
+			additionalInputParameters.add(DownloadedBytesGenerator.create(downloadedBytes));
+
+		if (downloadedDuration != null)
+			additionalInputParameters.add(DownloadedDurationGenerator.create(downloadedDuration));
+
+		return additionalInputParameters;
 	}
 
 	@Override
-	protected void handleSendTaskError(DelegateExecution execution, Variables variables, Exception exception,
-			String errorMessage)
+	public MessageSendTaskErrorHandler getErrorHandler()
 	{
-		Target target = variables.getTarget();
-		SendTaskErrorConverter.ProcessErrorWithStatusCode errorAndStatus = SendTaskErrorConverter
-				.convertLocal(exception, false, ConstantsPing.PROCESS_NAME_PING);
-
-		execution.setVariableLocal(ExecutionVariables.error.name(), new ProcessErrorValueImpl(errorAndStatus.error()));
-		execution.setVariableLocal(ExecutionVariables.statusCode.name(), CodeSystem.DsfPing.Code.ERROR.getValue());
-
-		logger.info("Request to {} resulted in error: {}", target.getEndpointUrl(),
-				errorAndStatus.error().concept().getDisplay());
-	}
-
-	@Override
-	protected void sendTask(DelegateExecution execution, Variables variables, Target target,
-			String instantiatesCanonical, String messageName, String businessKey, String profile,
-			Stream<Task.ParameterComponent> additionalInputParameters)
-	{
-		Target newTarget = new Target()
+		return new MessageSendTaskErrorHandler()
 		{
 			@Override
-			public String getOrganizationIdentifierValue()
+			public ErrorBoundaryEvent handleErrorBoundaryEvent(ProcessPluginApi processPluginApi, Variables variables,
+					ErrorBoundaryEvent errorBoundaryEvent)
 			{
-				return target.getOrganizationIdentifierValue();
+				return errorBoundaryEvent;
 			}
 
 			@Override
-			public String getEndpointIdentifierValue()
+			public Exception handleException(ProcessPluginApi processPluginApi, Variables variables,
+					SendTaskValues sendTaskValues, Exception e)
 			{
-				return target.getEndpointIdentifierValue();
-			}
+				Target target = variables.getTarget();
+				SendTaskErrorConverter.ProcessErrorWithStatusCode errorAndStatus = SendTaskErrorConverter
+						.convertLocal(e, false, ConstantsPing.PROCESS_NAME_PING);
 
-			@Override
-			public String getEndpointUrl()
-			{
-				return target.getEndpointUrl();
-			}
+				variables.setJsonVariableLocal(ExecutionVariables.error.name(),errorAndStatus.error());
+				variables.setStringLocal(ExecutionVariables.statusCode.name(), CodeSystem.DsfPing.Code.ERROR.getValue());
 
-			@Override
-			public String getCorrelationKey()
-			{
-				return null;
+				logger.info("Request to {} resulted in error: {}", target.getEndpointUrl(),
+						errorAndStatus.error().concept().getDisplay());
+
+				return e;
 			}
 		};
+	}
 
-		super.sendTask(execution, variables, newTarget, instantiatesCanonical, messageName, businessKey, profile,
-				additionalInputParameters);
+	@Override
+	public TaskSender getTaskSender(ProcessPluginApi api, Variables variables, SendTaskValues sendTaskValues)
+	{
+		return new DefaultTaskSender(api, variables, sendTaskValues, this.getBusinessKeyStrategy()) {
+			@Override
+			protected Target getTarget()
+			{
+				Target oldTarget = super.getTarget();
+				return new Target()
+				{
+					@Override
+					public String getOrganizationIdentifierValue()
+					{
+						return oldTarget.getOrganizationIdentifierValue();
+					}
+
+					@Override
+					public String getEndpointIdentifierValue()
+					{
+						return oldTarget.getEndpointIdentifierValue();
+					}
+
+					@Override
+					public String getEndpointUrl()
+					{
+						return oldTarget.getEndpointUrl();
+					}
+
+					@Override
+					public String getCorrelationKey()
+					{
+						return null;
+					}
+				};
+			}
+		};
 	}
 }
