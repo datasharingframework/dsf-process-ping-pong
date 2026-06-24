d5bf662fc8fc161628c4fc2fbc0e94f3d2c1f0ae
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-24 16:59:25 +0200

    API v2 changes: - interface instead of superclass -> no constructor needed - doExecute now named execute

--- src/main/java/dev/dsf/bpe/service/ping/LogAndSaveSendError.java
+++ src/main/java/dev/dsf/bpe/service/ping/LogAndSaveSendError.java
@@ -2,40 +2,33 @@ package dev.dsf.bpe.service.ping;
 
 import java.util.Objects;
 
-import org.camunda.bpm.engine.delegate.BpmnError;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.CodeSystem;
 import dev.dsf.bpe.ExecutionVariables;
 import dev.dsf.bpe.ProcessError;
-import dev.dsf.bpe.service.AbstractService;
 import dev.dsf.bpe.util.ErrorListUtils;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.variables.Variables;
-import dev.dsf.bpe.variables.codesystem.dsfpingstatus.CodeValueImpl;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.ServiceTask;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class LogAndSaveSendError extends AbstractService
+public class LogAndSaveSendError implements ServiceTask
 {
 	private static final Logger logger = LoggerFactory.getLogger(LogAndSaveSendError.class);
 
-	public LogAndSaveSendError(ProcessPluginApi api)
-	{
-		super(api);
-	}
-
 	@Override
-	protected void doExecuteWithErrorHandling(DelegateExecution execution, Variables variables) throws BpmnError
+	public void execute(ProcessPluginApi processPluginApi, Variables variables) throws ErrorBoundaryEvent, Exception
 	{
 		String correlationKey = variables.getTarget().getCorrelationKey();
-		ProcessError error = (ProcessError) execution.getVariableLocal(ExecutionVariables.error.name());
-		CodeSystem.DsfPingStatus.Code status = (CodeSystem.DsfPingStatus.Code) execution
+		ProcessError error = variables.getVariableLocal(ExecutionVariables.error.name());
+		CodeSystem.DsfPingStatus.Code status = variables
 				.getVariableLocal(ExecutionVariables.statusCode.name());
 		Objects.requireNonNull(status, "status");
 
-		ErrorListUtils.add(error, execution, correlationKey);
-		variables.setVariable(ExecutionVariables.statusCode.correlatedValue(correlationKey), new CodeValueImpl(status));
+		ErrorListUtils.add(error, variables, correlationKey);
+		variables.setJsonVariable(ExecutionVariables.statusCode.correlatedValue(correlationKey), status);
 		logger.debug("Saved error when trying to send ping message. Error message: {}", error.concept().getDisplay());
 	}
 }
