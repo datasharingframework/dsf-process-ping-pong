9613e9f06e3c2ccbeeffff68228824b0a902ba86
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-24 16:53:58 +0200

    API v2 changes: - interface instead of superclass -> no constructor needed - doExecute now named execute

--- src/main/java/dev/dsf/bpe/service/ping/LogAndSaveError.java
+++ src/main/java/dev/dsf/bpe/service/ping/LogAndSaveError.java
@@ -1,36 +1,30 @@
 package dev.dsf.bpe.service.ping;
 
-import org.camunda.bpm.engine.delegate.BpmnError;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.ExecutionVariables;
 import dev.dsf.bpe.ProcessError;
-import dev.dsf.bpe.service.AbstractService;
 import dev.dsf.bpe.util.ErrorListUtils;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.variables.Target;
-import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.ServiceTask;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.variables.Target;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class LogAndSaveError extends AbstractService
+public class LogAndSaveError implements ServiceTask
 {
 	private static final Logger logger = LoggerFactory.getLogger(LogAndSaveError.class);
 
-	public LogAndSaveError(ProcessPluginApi api)
-	{
-		super(api);
-	}
-
 	@Override
-	protected void doExecuteWithErrorHandling(DelegateExecution delegateExecution, Variables variables) throws BpmnError
+	public void execute(ProcessPluginApi processPluginApi, Variables variables) throws ErrorBoundaryEvent, Exception
 	{
 		Target target = variables.getTarget();
 
-		ProcessError error = (ProcessError) delegateExecution
+		ProcessError error = variables
 				.getVariableLocal(ExecutionVariables.resourceDownloadError.name());
 
-		ErrorListUtils.add(error, delegateExecution, target.getCorrelationKey());
+		ErrorListUtils.add(error, variables, target.getCorrelationKey());
 
 		logger.info("Error while trying to download resource from {}: {}", target.getEndpointUrl(),
 				error.concept().getDisplay());
