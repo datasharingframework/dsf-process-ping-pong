ff2f4046e04beeb12cae5027d615e9d340c9092b
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-25 13:17:52 +0200

    API v2 changes: - interface instead of superclass -> no api in constructor needed - doExecute renamed to execute

--- src/main/java/dev/dsf/bpe/service/pong/LogAndSaveAndStoreError.java
+++ src/main/java/dev/dsf/bpe/service/pong/LogAndSaveAndStoreError.java
@@ -1,45 +1,39 @@
 package dev.dsf.bpe.service.pong;
 
-import org.camunda.bpm.engine.delegate.BpmnError;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.ExecutionVariables;
 import dev.dsf.bpe.ProcessError;
-import dev.dsf.bpe.service.AbstractService;
 import dev.dsf.bpe.util.ErrorListUtils;
 import dev.dsf.bpe.util.task.output.generator.PingStatusGenerator;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.variables.Target;
-import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.ServiceTask;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.variables.Target;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class LogAndSaveAndStoreError extends AbstractService
+public class LogAndSaveAndStoreError implements ServiceTask
 {
 	private static final Logger logger = LoggerFactory.getLogger(LogAndSaveAndStoreError.class);
 
-	public LogAndSaveAndStoreError(ProcessPluginApi api)
-	{
-		super(api);
-	}
-
 	@Override
-	protected void doExecuteWithErrorHandling(DelegateExecution delegateExecution, Variables variables) throws BpmnError
+	public void execute(ProcessPluginApi processPluginApi, Variables variables) throws ErrorBoundaryEvent, Exception
 	{
 		Target target = variables.getTarget();
 		Task startTask = variables.getStartTask();
 
-		ProcessError error = (ProcessError) delegateExecution
+		ProcessError error = variables
 				.getVariable(ExecutionVariables.resourceDownloadError.name());
-		ErrorListUtils.add(error, delegateExecution);
+		ErrorListUtils.add(error, variables);
 
-		ProcessError errorRemote = (ProcessError) delegateExecution
+		ProcessError errorRemote = variables
 				.getVariable(ExecutionVariables.resourceDownloadErrorRemote.name());
-		ErrorListUtils.addRemote(errorRemote, delegateExecution);
+		ErrorListUtils.addRemote(errorRemote, variables);
 
 		PingStatusGenerator.updatePongStatusOutput(startTask,
-				ErrorListUtils.getErrorList(delegateExecution).getEntries());
+				ErrorListUtils.getErrorList(variables).getEntries());
 		variables.updateTask(startTask);
 
 		logger.info("Error while trying to download resource from {}: {}", target.getEndpointUrl(),
