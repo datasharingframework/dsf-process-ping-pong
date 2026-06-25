cecfad7d62a81638e2e3bce76b22248b50668c32
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-25 14:57:58 +0200

    API v2 changes: - interface instead of superclass -> no api in constructor needed - doExecute renamed to execute

--- src/main/java/dev/dsf/bpe/service/pong/StoreDownloadSpeed.java
+++ src/main/java/dev/dsf/bpe/service/pong/StoreDownloadSpeed.java
@@ -4,38 +4,36 @@ import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.Objects;
 
-import org.camunda.bpm.engine.delegate.BpmnError;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.CodeSystem;
 import dev.dsf.bpe.ExecutionVariables;
-import dev.dsf.bpe.service.AbstractService;
 import dev.dsf.bpe.util.task.output.generator.PingStatusGenerator;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.ServiceTask;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class StoreDownloadSpeed extends AbstractService
+public class StoreDownloadSpeed implements ServiceTask
 {
 	private static final Logger logger = LoggerFactory.getLogger(StoreDownloadSpeed.class);
 	private CodeSystem.DsfPingUnits.Code networkSpeedUnit;
 
-	public StoreDownloadSpeed(ProcessPluginApi api, CodeSystem.DsfPingUnits.Code networkSpeedUnit)
+	public StoreDownloadSpeed(CodeSystem.DsfPingUnits.Code networkSpeedUnit)
 	{
-		super(api);
 		this.networkSpeedUnit = networkSpeedUnit;
 	}
 
 	@Override
-	protected void doExecuteWithErrorHandling(DelegateExecution execution, Variables variables) throws BpmnError
+	public void execute(ProcessPluginApi processPluginApi, Variables variables) throws ErrorBoundaryEvent, Exception
 	{
 		Task startTask = variables.getStartTask();
 		logger.debug("Storing download speed...");
 
 		long downloadedBytes = variables.getLong(ExecutionVariables.downloadedBytes.name());
-		Duration downloadedDuration = (Duration) variables.getVariable(ExecutionVariables.downloadedDuration.name());
+		Duration downloadedDuration = variables.getVariable(ExecutionVariables.downloadedDuration.name());
 
 		BigDecimal downloadSpeed = null;
 		if (downloadedDuration != null)
