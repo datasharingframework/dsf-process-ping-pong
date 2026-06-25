73b2676d704f910f189d03938c69754b9873fd78
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-25 13:34:50 +0200

    API v2 changes: - interface instead of superclass -> no api in constructor needed - doExecute renamed to execute

--- src/main/java/dev/dsf/bpe/service/pong/SaveTimeoutError.java
+++ src/main/java/dev/dsf/bpe/service/pong/SaveTimeoutError.java
@@ -1,36 +1,30 @@
 package dev.dsf.bpe.service.pong;
 
-import org.camunda.bpm.engine.delegate.BpmnError;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dev.dsf.bpe.CodeSystem;
 import dev.dsf.bpe.ConstantsPing;
 import dev.dsf.bpe.ProcessError;
-import dev.dsf.bpe.service.AbstractService;
 import dev.dsf.bpe.util.ErrorListUtils;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.ServiceTask;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class SaveTimeoutError extends AbstractService
+public class SaveTimeoutError implements ServiceTask
 {
 	private static final Logger logger = LoggerFactory.getLogger(SaveTimeoutError.class);
 
-	public SaveTimeoutError(ProcessPluginApi api)
-	{
-		super(api);
-	}
-
 	@Override
-	protected void doExecuteWithErrorHandling(DelegateExecution execution, Variables variables) throws BpmnError
+	public void execute(ProcessPluginApi processPluginApi, Variables variables) throws ErrorBoundaryEvent, Exception
 	{
 		logger.debug("Storing timeout error...");
 
 		ProcessError error = new ProcessError(ConstantsPing.PROCESS_NAME_PONG,
 				CodeSystem.DsfPingError.Concept.CLEANUP_MESSAGE_TIMEOUT, null);
 
-		ErrorListUtils.add(error, execution);
+		ErrorListUtils.add(error, variables);
 
 		logger.debug("Stored timeout error: {}", error.concept().getDisplay());
 	}
