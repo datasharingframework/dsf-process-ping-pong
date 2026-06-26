8c3cdf84174287ba21fefb731796c1b85c8e61ae
Jan Böhringer <jan.boehringer@gmx.de>
2026-06-26 08:52:52 +0200

    API v2 changes: - interface instead of superclass -> no api in constructor needed - doExecute renamed to execute

--- src/main/java/dev/dsf/bpe/service/pong/StoreErrors.java
+++ src/main/java/dev/dsf/bpe/service/pong/StoreErrors.java
@@ -3,54 +3,50 @@ package dev.dsf.bpe.service.pong;
 import java.util.Map;
 import java.util.Objects;
 
-import org.camunda.bpm.engine.delegate.BpmnError;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 
 import dev.dsf.bpe.CodeSystem;
 import dev.dsf.bpe.ExecutionVariables;
 import dev.dsf.bpe.ProcessErrors;
 import dev.dsf.bpe.mail.AggregateErrorMailService;
-import dev.dsf.bpe.service.AbstractService;
 import dev.dsf.bpe.util.ErrorListUtils;
 import dev.dsf.bpe.util.task.output.generator.PingStatusGenerator;
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.variables.Target;
-import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.ServiceTask;
+import dev.dsf.bpe.v2.error.ErrorBoundaryEvent;
+import dev.dsf.bpe.v2.variables.Target;
+import dev.dsf.bpe.v2.variables.Variables;
 
-public class StoreErrors extends AbstractService
+public class StoreErrors implements ServiceTask, InitializingBean
 {
 	private static final Logger logger = LoggerFactory.getLogger(StoreErrors.class);
 
 	private final AggregateErrorMailService errorMailService;
 
-	public StoreErrors(ProcessPluginApi api, AggregateErrorMailService errorMailService)
+	public StoreErrors(AggregateErrorMailService errorMailService)
 	{
-		super(api);
-
 		this.errorMailService = errorMailService;
 	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception
 	{
-		super.afterPropertiesSet();
-
 		Objects.requireNonNull(errorMailService, "errorMailService");
 	}
 
 	@Override
-	protected void doExecuteWithErrorHandling(DelegateExecution execution, Variables variables) throws BpmnError
+	public void execute(ProcessPluginApi processPluginApi, Variables variables) throws ErrorBoundaryEvent, Exception
 	{
 		Task startTask = variables.getStartTask();
 		logger.debug("Storing errors...");
 
-		ProcessErrors errors = ErrorListUtils.getErrorList(execution);
+		ProcessErrors errors = ErrorListUtils.getErrorList(variables);
 		PingStatusGenerator.updatePongStatusOutput(startTask, errors.getEntries());
 
-		CodeSystem.DsfPingStatus.Code status = (CodeSystem.DsfPingStatus.Code) variables
+		CodeSystem.DsfPingStatus.Code status = variables
 				.getVariable(ExecutionVariables.statusCode.name());
 		PingStatusGenerator.updatePongStatusOutput(startTask, status);
 
